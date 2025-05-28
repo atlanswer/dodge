86418339760573c326a72a98af08d0ed117c8b29
Atlanswer <atlanswer@gmail.com>
2025-05-29 03:42:07 +0800

    initialize postlandmarker

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
 import { FilesetResolver, PoseLandmarker } from "@mediapipe/tasks-vision";
+import type React from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [poseLandmarker, setPoseLandmarker] = useState<PoseLandmarker>();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Trust me bro
+  useEffect(() => {
+    createPostLandmarker().then((res) => {
+      setPoseLandmarker(res);
+    });
+    return () => {
+      poseLandmarker?.close();
+    };
+  }, []);
+
+  async function handleClick(e: React.MouseEvent | React.KeyboardEvent) {
+    if (
+      e.type === "keydown" &&
+      (e as React.KeyboardEvent).key !== " " &&
+      (e as React.KeyboardEvent).key !== "Enter"
+    ) {
+      return;
+    }
+    if (poseLandmarker === undefined) {
+      console.warn("postLandmarker not initialized yet");
+    }
+    console.debug("triggered");
+  }
+
   return (
-    <div className="mx-auto flex flex-col place-items-center gap-4">
-      <img
-        src="https://assets.codepen.io/9177687/woman-ge0f199f92_640.jpg"
-        className="mt-4 rounded"
-        width={640}
-        crossOrigin="anonymous"
+    <div className="mx-auto flex flex-col place-items-center gap-8">
+      <div
+        className={`mt-10 rounded px-4 py-2 outline ${poseLandmarker ? "bg-green-800" : "bg-yellow-800"}`}
+      >
+        {poseLandmarker ? "Mediapipe Loaded" : "Loading Mediapipe"}
+      </div>
+      <button
+        className={`rounded ${poseLandmarker && "cursor-pointer hover:outline-4 focus:outline-4"}`}
+        onClick={handleClick}
+        onKeyDown={handleClick}
+        type="button"
         title="Click to get detection!"
-        alt="Post landmark detection sample 1"
-      />
-      <img
-        src="https://assets.codepen.io/9177687/woman-g1af8d3deb_640.jpg"
-        className="rounded"
-        width={640}
-        crossOrigin="anonymous"
+      >
+        <img
+          src="https://assets.codepen.io/9177687/woman-ge0f199f92_640.jpg"
+          className="rounded-[inherit]"
+          width={640}
+          crossOrigin="anonymous"
+          alt="Post landmark detection sample 1"
+        />
+      </button>
+      <button
+        className={`rounded ${poseLandmarker && "cursor-pointer hover:outline-4 focus:outline-4"}`}
+        onClick={handleClick}
+        onKeyDown={handleClick}
+        type="button"
         title="Click to get detection!"
-        alt="Post landmark detection sample 1"
-      />
+      >
+        <img
+          src="https://assets.codepen.io/9177687/woman-g1af8d3deb_640.jpg"
+          className="rounded-[inherit]"
+          width={640}
+          crossOrigin="anonymous"
+          title="Click to get detection!"
+          alt="Post landmark detection sample 1"
+        />
+      </button>
     </div>
   );
 }
