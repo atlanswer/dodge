6c1605c90ee23642385cf4c2a33243aebad33c7c
Atlanswer <atlanswer@gmail.com>
2025-05-30 10:41:55 +0800

    stacked layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { FilesetResolver, PoseLandmarker } from "@mediapipe/tasks-vision";
+import {
+  DrawingUtils,
+  FilesetResolver,
+  PoseLandmarker,
+} from "@mediapipe/tasks-vision";
 import type React from "react";
 import { useEffect, useState } from "react";
 
 export default function App() {
   const [poseLandmarker, setPoseLandmarker] = useState<PoseLandmarker>();
+  const [runningMode, setRunningMode] = useState<"IMAGE" | "VIDEO">("IMAGE");
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: Trust me bro
   useEffect(() => {
@@ -13,9 +18,28 @@ export default function App() {
     return () => {
       poseLandmarker?.close();
     };
+
+    async function createPostLandmarker() {
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm",
+      );
+      return await PoseLandmarker.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath:
+            "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+          delegate: "GPU",
+        },
+        runningMode: runningMode,
+        numPoses: 2,
+      });
+    }
   }, []);
 
-  async function handleClick(e: React.MouseEvent | React.KeyboardEvent) {
+  async function handleClick(
+    e:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLButtonElement>,
+  ) {
     if (
       e.type === "keydown" &&
       (e as React.KeyboardEvent).key !== " " &&
@@ -25,19 +49,27 @@ export default function App() {
     }
     if (poseLandmarker === undefined) {
       console.warn("postLandmarker not initialized yet");
+      return;
     }
-    console.debug("triggered");
+
+    if (runningMode === "VIDEO") {
+      await poseLandmarker.setOptions({ runningMode: "IMAGE" });
+      setRunningMode("IMAGE");
+    }
+
+    // @ts-expect-error: Trust me bro
+    const res = poseLandmarker.detect(e.target);
   }
 
   return (
-    <div className="mx-auto flex flex-col place-items-center gap-8">
+    <div className="mx-auto flex flex-col place-items-center gap-8 py-10">
       <div
-        className={`mt-10 rounded px-4 py-2 outline ${poseLandmarker ? "bg-green-800" : "bg-yellow-800"}`}
+        className={`rounded px-4 py-2 outline ${poseLandmarker ? "bg-green-800" : "bg-yellow-800"}`}
       >
         {poseLandmarker ? "Mediapipe Loaded" : "Loading Mediapipe"}
       </div>
       <button
-        className={`rounded ${poseLandmarker && "cursor-pointer hover:outline-4 focus:outline-4"}`}
+        className={`grid grid-cols-1 grid-rows-1 rounded ${poseLandmarker && "cursor-pointer hover:outline-4 focus:outline-4"}`}
         onClick={handleClick}
         onKeyDown={handleClick}
         type="button"
@@ -45,14 +77,15 @@ export default function App() {
       >
         <img
           src="https://assets.codepen.io/9177687/woman-ge0f199f92_640.jpg"
-          className="rounded-[inherit]"
+          className="col-start-1 row-start-1 rounded-[inherit]"
           width={640}
           crossOrigin="anonymous"
           alt="Post landmark detection sample 1"
         />
+        <canvas className="col-start-1 row-start-1 h-full w-full"></canvas>
       </button>
       <button
-        className={`rounded ${poseLandmarker && "cursor-pointer hover:outline-4 focus:outline-4"}`}
+        className={`grid grid-cols-1 grid-rows-1 rounded ${poseLandmarker && "cursor-pointer hover:outline-4 focus:outline-4"}`}
         onClick={handleClick}
         onKeyDown={handleClick}
         type="button"
@@ -60,28 +93,14 @@ export default function App() {
       >
         <img
           src="https://assets.codepen.io/9177687/woman-g1af8d3deb_640.jpg"
-          className="rounded-[inherit]"
+          className="col-start-1 row-start-1 rounded-[inherit]"
           width={640}
           crossOrigin="anonymous"
           title="Click to get detection!"
-          alt="Post landmark detection sample 1"
+          alt="Post landmark detection sample 2"
         />
+        <canvas className="col-start-1 row-start-1 h-full w-full"></canvas>
       </button>
     </div>
   );
-}
-
-async function createPostLandmarker() {
-  const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm",
-  );
-  return await PoseLandmarker.createFromOptions(vision, {
-    baseOptions: {
-      modelAssetPath:
-        "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
-      delegate: "GPU",
-    },
-    runningMode: "IMAGE",
-    numPoses: 2,
-  });
 }
