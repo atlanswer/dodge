cedf54f936e16536a8f6dc161f08a9c00b99b5d1
Atlanswer <atlanswer@gmail.com>
2025-08-14 10:33:35 +0800

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindCss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-oxc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
